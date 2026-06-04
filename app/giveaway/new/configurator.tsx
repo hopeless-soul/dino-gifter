611bae7a413a1a6c7f6ba4eca33789cc89971826
hopeless-soul <r3al.hum4n@gmail.com>
2026-06-04 13:21:25 +0200

    refactor: move game components to components/game

--- app/giveaway/new/configurator.tsx
+++ app/giveaway/new/configurator.tsx
@@ -11,7 +11,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { ServerTabs } from '@/components/ServerTabs'
+import { ServerTabs } from '@/components/game/ServerTabs'
 import { TrialConfigurator } from '@/components/TrialConfigurator'
 import type { InventoryItem, SlotCard, TrialData, Giveaway } from '@/lib/types'
 
